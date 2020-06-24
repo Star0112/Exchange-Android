c195b7cc3f67ffb33ed5ab66fbcb4e3615908297
star0112 <dreamstar30112@gmail.com>
2020-06-25 05:53:24 +1000

    add copy address

--- app/src/main/java/com/urgentrn/urncexchange/ui/adapter/CoinDepositAdapter.java
+++ app/src/main/java/com/urgentrn/urncexchange/ui/adapter/CoinDepositAdapter.java
@@ -1,8 +1,12 @@
 package com.urgentrn.urncexchange.ui.adapter;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.urgentrn.urncexchange.R;
 import com.urgentrn.urncexchange.models.AssetBalance;
 import com.urgentrn.urncexchange.models.DepositHistory;
+import com.urgentrn.urncexchange.ui.base.BaseActivity;
 import com.urgentrn.urncexchange.ui.holder.CoinDepositHolder;
 
 import java.util.ArrayList;
@@ -41,8 +46,14 @@ public class CoinDepositAdapter extends RecyclerView.Adapter<CoinDepositHolder> 
     public void onBindViewHolder(@NonNull CoinDepositHolder holder, int position) {
         final AssetBalance assetBalance = data.get(position);
         holder.updateView(assetBalance, position);
-        holder.itemView.setOnClickListener(v-> {
-            mListener.onItemClick(position);
+        holder.itemView.findViewById(R.id.btnAddress).setOnClickListener(v -> {
+            final ClipboardManager clipboard = (ClipboardManager)v.getContext().getSystemService((Context.CLIPBOARD_SERVICE));
+            final ClipData clipData = ClipData.newPlainText(v.getResources().getString(R.string.address), data.get(position).getAddress());
+            clipboard.setPrimaryClip(clipData);
+            Toast.makeText(v.getContext(), data.get(position).getAddress(), Toast.LENGTH_SHORT).show();
+        });
+        holder.itemView.findViewById(R.id.btnDeposit).setOnClickListener(v -> {
+
         });
     }
 
